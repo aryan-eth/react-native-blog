316cdbfceeadcc71f1b6c282a92c658afee12386
Aryan Singh <aryansingh@umass.edu>
2019-09-22 15:00:26 -0400

    install navigation

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ const App = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Learn More</Text>
               <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+                Read the Documentation to discover what to do next:
               </Text>
             </View>
             <LearnMoreLinks />
